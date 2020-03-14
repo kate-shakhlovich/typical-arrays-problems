cf8fb64041e3b527900d3178d94b94d9ba2e5a8c
Kate Shakhlovich <kate.shakhlovich@gmail.com>
2020-03-14 22:41:40 +0300

    feat:typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length === 0) return 0;
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length === 0) return 0;
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length === 0) return 0;
+  let sum = array.reduce((accum, elem) => accum + elem);
+  return sum / array.length;
 }
